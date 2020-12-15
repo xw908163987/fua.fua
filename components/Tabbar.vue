2e4b02645846afd71ee3adc7ef4bd343c1def816
vspace <908163987@qq.com>
2020-12-15 14:19:03 +0800

    1

---
+++ components/Tabbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tabbar-container">
+    <div class="item" v-for="(item,index) in routes" @click="select(index)" :key="item.path">
+      <nuxt-link :to="item.path" :class="[item.is_selected ? 'selected' : '']">{{ item.text }} </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      routes: [
+        {
+          text: "主页",
+          path: "/home",
+          is_selected:false
+        },
+        {
+          text: "发现",
+          path: "/shop_center/introduction",
+          is_selected:false
+        },
+        {
+          text: "地图",
+          path: "/map",
+          is_selected:false
+        },
+        {
+          text: "个人",
+          path: "/user/login",
+          is_selected:false
+        },
+      ],
+    };
+  },
+  methods:{
+    select(index){
+      this.routes.forEach(item=>{
+        item.is_selected = false
+      })
+      this.routes[index].is_selected = true
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.tabbar-container {
+  display: flex;
+  background: #fff;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1rem 0;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  .selected{
+  color: #FB8B8D;
+}
+}
+
+</style>
---
+++ pages/shop_center/introduction.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="introduction">
+    <div class="info">
+      <div class="image">
+        <img src="/shop_center/img1.jpg" alt="" />
+      </div>
+      <div class="desc">
+        <div class="title">藤原家按摩店</div>
+        <p>
+          河南彭世健康管理集团有限公司始创于2000年，前身为郑州彭世足部保健服务有限公司，是中国知名的足部健康产业企业。经过20年的发展，
+          集团成功打造特许连锁加盟经营模式，在全国地区加盟店达3000+家，打造“千城万店”战略规划。
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.introduction {
+  .info {
+    margin: 1rem;
+    background: #fff;
+    border-radius: 1rem;
+    overflow: hidden;
+  }
+  .image {
+    width: 100%;
+    img {
+      width: 100%;
+    }
+  }
+  .desc {
+      padding: 1rem;
+    .title {
+      width: 100%;
+      text-align: center;
+      font-weight: 600;
+      color: #000;
+      font-size: 1rem;
+      margin: 0 0 1rem 0;
+    }
+    p {
+      color: #6b6b6b;
+      
+    }
+  }
+}
+</style>
