ad8433c879fa651322e79137cb8f7f36ed502af6
vspace <908163987@qq.com>
2020-12-18 09:35:50 +0800

    修改config配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,16 +28,17 @@ export default {
   /*
   ** Global CSS
   */
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+ css: [
+  'vant/lib/index.css',
+  '@/assets/reset.css'
+],
+/*
+** Plugins to load before mounting the App
+** https://nuxtjs.org/guide/plugins
+*/
+plugins: [
+  '@/plugins/vant'
+],
   /*
   ** Auto import components
   ** See https://nuxtjs.org/api/configuration-components
@@ -58,6 +59,6 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
-    transpile: [/^element-ui/],
+    transpile: [/^vant/],
   }
 }
