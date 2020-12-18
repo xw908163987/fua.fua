4cc11a1220878479b9c725fb1382d29ac6fab0ce
vspace <908163987@qq.com>
2020-12-18 17:32:35 +0800

    引入reset

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,5 +7,9 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    require("@/assets/reset.css");
+  },
+};
 </script>
--- layouts/login.vue
+++ layouts/login.vue
@@ -5,7 +5,11 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    require("@/assets/reset.css");
+  },
+};
 </script>
 
 <style lang="less" scoped></style>
